b44e56e210a98fca00a2e21ee261ce81430f333a
Patience Jelagat <patiencejelagatt@gmail.com>
2026-06-16 18:40:59 +0300

    Two-state navbar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// MENU BUTTON FUNCTIONALITY
 const menuBtn = document.getElementById('menuBtn');
 const menu = document.querySelector('.menu');
 
@@ -11,4 +12,22 @@ document.addEventListener('click', (e) => {
 
     if(!clickedInsideMenu && !clickedMenuBtn)
         menu.classList.remove('show');
+});
+
+
+// NAVBAR STATE CHANGE WHEN SCROLLING
+const nav = document.querySelector('nav');
+const links = document.querySelector('.links');
+const bookBtn = document.querySelector('.bookBtn');
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY > 50){
+        nav.classList.add('scrolled');
+        links.classList.add('scrolled');
+        bookBtn.classList.add('scrolled');
+    }else{
+        nav.classList.remove('scrolled');
+        links.classList.remove('scrolled');
+        bookBtn.classList.remove('scrolled');
+    }
 });
